99d00892e1b443100b36ec2599cac19e829da17b
CLANDEET <jskim3558@gmail.com>
2015-02-16 22:48:00 +0900

    nester

--- nester1.py
+++ nester1.py
@@ -1,4 +1,6 @@
 """This is the "nester.py" module and it provides one function called print_lol() which prints lists that may or may not include nested lists."""
+#github testing
+
 
 import sys
 def print_lol(the_list, indent=False, level=0, fh=sys.stdout):
